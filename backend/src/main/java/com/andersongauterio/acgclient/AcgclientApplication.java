fb4993e965a090c7027f05c2539b99ecf1ad4a3f
Andersongauterio <anderson.camara.gauterio@gmail.com>
2022-05-19 10:15:41 -0300

    First commit

---
+++ backend/src/main/java/com/andersongauterio/acgclient/AcgclientApplication.java
@@ -0,0 +1,13 @@
+package com.andersongauterio.acgclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcgclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcgclientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/andersongauterio/acgclient/AcgclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andersongauterio.acgclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcgclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
